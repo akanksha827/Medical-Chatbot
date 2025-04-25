81da667f1fc420a4b24f72690c752e031836e619
akanksha827 <akankshang2004@gmail.com>
2025-04-25 21:38:35 +0530

    commit

--- Medical-chatbot-using-LLM-main/src/helper.py
+++ Medical-chatbot-using-LLM-main/src/helper.py
@@ -1,24 +1,17 @@
-from langchain import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Pinecone
-import pinecone
-from langchain.document_loaders import PyPDFLoader , DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
-def load_pdf(data):
-    
-    loader = DirectoryLoader(data, glob="*.pdf", loader_cls=PyPDFLoader)
+def load_pdf(data_path):
+    loader = DirectoryLoader(data_path, glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
     return documents
 
 def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500 , chunk_overlap = 20)
-    text_chunks=text_splitter.split_documents(extracted_data)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(extracted_data)
     return text_chunks
 
 def download_hugging_face_embeddings():
-    embeddings = HuggingFaceEmbeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embeddings
